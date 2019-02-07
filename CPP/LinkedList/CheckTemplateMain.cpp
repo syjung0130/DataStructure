839d61e50aff921b140e22edfd257d67d211a9e3
unlocker-sy <soonyoung87@gmail.com>
2019-02-07 15:19:16 +0900

    template function 샘플 코드 추가

---
+++ CPP/LinkedList/CheckTemplateMain.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+using namespace std;
+
+// function template
+template <typename T>
+T GetMax (T a, T b) {
+    T result;
+    result = (a>b)? a : b;
+    return (result);
+}
+
+template <typename T>
+T GetMul (T a, T b)
+{
+    T result;
+    result = a/b;
+    return result;
+}
+
+int main () 
+{
+    int i=5, j=6, k;
+    long l=10, m=5, n;
+    cout << "==== Get Max Value ====" << endl;
+    k=GetMax<int>(i,j);
+    n=GetMax<long>(l,m);
+    cout << k << endl;
+    cout << n << endl;
+
+    cout << "==== Get Mul Value =====" << endl;
+    int iOp1 = 5, iOp2 = 5;
+    int iResult = 0;
+    double dOp1 = 5.0, dOp2 = 5.0;
+    double dResult = 0.0;
+
+    iResult = GetMul<int>(iOp1, iOp2);
+    dResult = GetMul<double>(dOp1, dOp2);
+    // dResult = 10.99999;
+    cout.precision(5);
+    cout << std::fixed << "int multiply result: " << iResult << endl
+            << "double multiply result: " << dResult << endl;
+
+  return 0;
+}
